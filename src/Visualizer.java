015031f93e926f731f49792278110d157d44ad6c
jsluke <jluke92@gmail.com>
2015-03-08 11:33:11 -0700

    Delete binary
    oops

--- src/Visualizer.java
+++ src/Visualizer.java
@@ -1,4 +1,24 @@
+import java.util.Hashtable;
+
 
 public class Visualizer {
+
+	private static Hashtable<Long,Thread> ThreadTable;
+
+	public Visualizer()
+	{
+		ThreadTable = new Hashtable<Long, Thread>();
+	}
+
+	//Threads will call this method to add themselves to the ArrayList
+	public void addThread(Thread th)
+	{
+		ThreadTable.put(th.getId(), th);
+	}
+
+	public Thread getThread(long id)
+	{
+		return ThreadTable.get(id);
+	}
 
 }
