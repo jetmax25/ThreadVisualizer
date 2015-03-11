5e37efbd132a172e89ea04706bbc77d08a591026
jsluke <jluke92@gmail.com>
2015-03-11 01:51:38 -0400

    Basic data collection functionality
    Visualizer: add log functionality and recurring collection task
    functionality
    
    DataCollectionTask: A class that extends TimerTask - will be scheduled
    at a specified tickRate by Visualizer
    
    LogElement: struct like class for storing log information in the
    arraylist withing Visualizer

--- src/Visualizer.java
+++ src/Visualizer.java
@@ -1,24 +1,86 @@
+/*
+*	Gathers and stores system information (CPU%, Memory use, etc) at a specified tickRate
+*	Stores all of the information repoted by threads via the logging function
+*	Makes all of this information available to the UI system
+*/
+
+
 import java.util.Hashtable;
+import java.util.ArrayList;
+import java.sql.Timestamp;
+import java.util.Timer;
 
 
-public class Visualizer {
+public final class Visualizer {
 
-	private static Hashtable<Long,Thread> ThreadTable;
+	private static Hashtable<Long,Thread> threadTable;
 
-	public Visualizer()
+	//stores all of the events called by threads
+	private static ArrayList<LogElement> logList;
+
+	//rate at which the Visualizer logs CPU/Memory Usage, watched variables, etc.
+	private static int tickRate;
+
+	//kicks off our data collection at every tickRate interval
+	private static Timer collectionTimer;
+	private static boolean timerStarted;
+
+	private Visualizer()
 	{
-		ThreadTable = new Hashtable<Long, Thread>();
+		threadTable = new Hashtable<Long, Thread>();
+		logList = new ArrayList<LogElement>();
+
+		tickRate = 500;
+
+		collectionTimer = new Timer();
+		timerStarted = false;
 	}
 
-	//Threads will call this method to add themselves to the ArrayList
-	public void addThread(Thread th)
+	//Threads will call this method to add themselves to the threadTable
+	public static void addThread(Thread th)
 	{
-		ThreadTable.put(th.getId(), th);
+		threadTable.put(th.getId(), th);
 	}
 
-	public Thread getThread(long id)
+	public static Thread getThread(long id)
 	{
-		return ThreadTable.get(id);
+		return threadTable.get(id);
+	}
+
+	public static void setTickRate(int rateInMS)
+	{
+		tickRate = rateInMS;
+	}
+
+	public static void logEvent(Long threadID, String message, Timestamp time)
+	{
+		logList.add(new LogElement(threadID, message, time));
+	}
+
+	public static void printLog()
+	{
+		for(LogElement entry : logList)
+		{
+			System.out.println(entry.time.toString() + " - "
+				+ entry.threadID + " - " + entry.message);
+		}
+	}
+
+	public static void startDataCollection()
+	{
+		//if already started, cancel the tasks and restart (possibly at new rate)
+		if(timerStarted)
+		{
+			stopDataCollection();
+		}
+
+		collectionTimer.scheduleAtFixedRate(new DataCollectionTask(), 0, tickRate);
+	}
+
+	public static void stopDataCollection()
+	{
+		collectionTimer.cancel();
+		collectionTimer.purge();
 	}
 
 }
