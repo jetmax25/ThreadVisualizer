7c8d4597cbb54141afb107f9020b83a114e1309c
Michael Pittman <m.pittman@knights.ucf.edu>
2015-03-17 20:18:08 -0400

    Created a thread for the entire gui to run in. Should have done it to begin with.

--- src/VisualGUI.java
+++ src/VisualGUI.java
@@ -33,7 +33,6 @@ import org.jfree.data.xy.XYSeriesCollection;
 
 
 public class VisualGUI {
-	
 	XYSeriesCollection dataset;
 	XYSeriesCollection dataset2;
 	TaskSeriesCollection data4;
@@ -68,240 +67,247 @@ public class VisualGUI {
 	XYBarRenderer renderer4;
 	
 	public VisualGUI(){
-					// create datasets
-					dataset = new XYSeriesCollection();	
-					
-					dataset2 = new XYSeriesCollection();
-					
-					data4 = new TaskSeriesCollection();
-					dataset4 = new XYTaskDataset(data4);
-					
-					
-					// create charts
-					chart = ChartFactory.createXYLineChart("CPU Usage", "Time", "Percentage", dataset, PlotOrientation.VERTICAL, true, true, false);
-					currChart = chart;
-					
-					plot1 = (XYPlot) chart.getPlot();
-					domain1 = (NumberAxis) plot1.getDomainAxis();
-					domain1.setRange(0, 10);
-					
-					chart2 = ChartFactory.createXYLineChart("Memory Usage", "Time", "Percentage", dataset, PlotOrientation.VERTICAL, true, true, false);
-					
-					chart4 = ChartFactory.createXYBarChart("Critical Sections", "Thread", false, "Time", dataset4, PlotOrientation.HORIZONTAL, true, true, false);
-					
-					
-					//create labels for the critical sections chart y-axis
-					String[] chart4AxisLabels = new String[getNumberOfThreads()];
-					for(int j=0; j<chart4AxisLabels.length; j++){
-						chart4AxisLabels[j] = "Thread " + (j+1);
-					}
-					SymbolAxis symbolaxis = new SymbolAxis("Series", chart4AxisLabels);
-					
-					//set the domain and range of the critical sections axes
-					plot4 = chart4.getXYPlot();
-					plot4.setDomainAxis(symbolaxis);
-					plot4.setRangeAxis(new DateAxis("Time"));
-					
-					//not entirely sure why we need these three lines but its started working when I put them in
-					renderer4 = (XYBarRenderer) plot4.getRenderer();
-					renderer4.setUseYInterval(true);
-					ChartUtilities.applyCurrentTheme(chart4);
-					
-					
-					//this is for chart4 *****************************************************************************
-					taskSeriesArray = new TaskSeries[getNumberOfThreads()];										//****
-					for(int i=0; i<taskSeriesArray.length; i++){												//****
-						taskSeriesArray[i] = new TaskSeries("Thread" + Integer.toString(i+1));					//****
-																												//****
-						//The Task object has the parameters Task(String, Date start, Date end)					//****
-						//The class Date represents a specific instant in time, with millisecond precision.		//****
-						//Make sure to cast the Date parameter to a long 										//****
-						taskSeriesArray[i].add(new Task("herro", new Date(2*i), new Date(3*i)));				//****
-						data4.add(taskSeriesArray[i]);															//****
-					}																							//****
-					//************************************************************************************************
-					
-					// create a panel to put the chart in
-					chartPanel = new ChartPanel(chart);
-					
-					//This is for chart1 *****************************************************
-					seriesArray = new XYSeries[getNumberOfThreads()];                     //**
-					for(int i = 0; i<seriesArray.length; i++){                            //**
-						seriesArray[i] = new XYSeries("Thread" + Integer.toString(i+1));  //**
-						dataset.addSeries(seriesArray[i]);								  //**
-					}																	  //**
-					//************************************************************************
-					
-					chartPanel.setVisible(true);
-						
-					
-					//creating JButtons for JPanel 1
-					JButton button1 = new JButton("CPU Visualizer");
-					JButton button2 = new JButton("Memory Visualizer");
-					JButton button3 = new JButton("Thread Sleep Time");
-					JButton button4 = new JButton("Thread Critical Sections");
-					
-								
-					button1.setMaximumSize(new Dimension(200, 200));
-					button2.setMaximumSize(new Dimension(200, 200));
-					button3.setMaximumSize(new Dimension(200, 200));
-					button4.setMaximumSize(new Dimension(200, 200));
-					
-					//add action listeners to each button
-					button1.addActionListener(new ActionListener(){
-						public void actionPerformed(ActionEvent e){
-							chartPanel = new ChartPanel(chart);
-							jpanel2.removeAll();
-							jpanel2.add(chartPanel);
-							jpanel2.revalidate();
-							jpanel2.repaint();
-							currChart = chart;
-						}
-					});
-					
-					
-					
-					
-					button2.addActionListener(new ActionListener(){
-						public void actionPerformed(ActionEvent e){
-							chartPanel2 = new ChartPanel(chart2);
-							jpanel2.removeAll();
-							jpanel2.add(chartPanel2);
-							jpanel2.revalidate();
-							jpanel2.repaint();
-							currChart = chart2;
-						}
-					});
-					
-					
-					button3.addActionListener(new ActionListener(){
-						public void actionPerformed(ActionEvent e){
-							//add code here
-						}
-					});
-					
-					
-					button4.addActionListener(new ActionListener(){
-						public void actionPerformed(ActionEvent e){
-							chartPanel4 = new ChartPanel(chart4);
-							jpanel2.removeAll();
-							jpanel2.add(chartPanel4);
-							jpanel2.revalidate();
-							jpanel2.repaint();
-							currChart = chart4;
-						}
-					});
-					
-					
-					
-					
-					
-					//creating JPanels
-					jpanel1 = new JPanel();
-					jpanel1.setLayout(new BoxLayout(jpanel1, BoxLayout.Y_AXIS));
-					
-					jpanel2 = new JPanel();
-					jpanel3 = new JPanel();			
-					
-					//adding buttons to JPanel1
-					jpanel1.add(button1);
-					jpanel1.add(button2);
-					jpanel1.add(button3);
-					jpanel1.add(button4);
-					
-					jpanel1.setVisible(true);
-					
-					
-					//adding the chartPanel with the JFreeChart inside to JPanel2
-					jpanel2.add(chartPanel);
-					jpanel2.setVisible(true);
-					
-					//adding checkboxes to jpanel3
-					int numThreads = getNumberOfThreads();
-					checkboxes = new JCheckBox[numThreads];
-					
-					int i;
-					for(i=0; i<numThreads; i++){
-						checkboxes[i] = new JCheckBox("Thread " + Integer.toString(i+1), true);
-						checkboxes[i].addActionListener(new ActionListener(){
-							public void actionPerformed(ActionEvent e){
-								//remove data series here
-								JCheckBox thisBox = (JCheckBox) e.getSource();
-								if(thisBox.isSelected() == false){
-									removeSeries(thisBox.getText());
-								}
-								else{
-									addSeries(thisBox.getText());
-								}
-							}
-						});
-						jpanel3.add(checkboxes[i]);
-					}
-					jpanel3.setVisible(true);
-					
-					
-					
-					//Adding components to JFrame using GridBagLayout manager
-					jframe = new JFrame("Concurrent Visualizer");
-					jframe.setLayout(new GridBagLayout());
-					jframe.setResizable(false);
-					jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-					
-					gc = new GridBagConstraints();
-					gc.gridx = 0;
-					gc.gridy = 0;
-					gc.gridwidth = 1;
-					gc.gridheight = 1;
-					gc.weightx = 10;
-					gc.weighty = 10;
-					gc.insets = new Insets(0, 0, 0, 0);
-					gc.anchor = GridBagConstraints.FIRST_LINE_START;
-					gc.fill = GridBagConstraints.BOTH;
-					
-					
-					gc.gridheight = 3;
-					gc.gridwidth = 1;
-					jframe.add(jpanel1, gc);
-					
-					
-					gc.gridx = 1;
-					gc.gridy = 0;
-					gc.gridheight = 2;
-					gc.gridwidth = 2;
-					jframe.add(jpanel2, gc);
-					
-					gc.gridx = 1;
-					gc.gridy = 2;
-					gc.gridheight = 1;
-					gc.gridwidth = 2;
-					jframe.add(jpanel3, gc);
-					
-					
-					jframe.setSize(900, 600);
-					jframe.setVisible(true);
-					
-					
-					Thread thread = new Thread(new Runnable(){
+		
+					Thread guiThread = new Thread(new Runnable(){
 						public void run(){
-							int counter = 0;
-							int i;
-							while(counter < 100){
-								for(i=0; i<seriesArray.length; i++){
-									seriesArray[i].add(counter, returnRandom());
-									if(counter > 10)
-										domain1.setRange(counter-10, counter);
-								}
-								counter++;
-								try{Thread.sleep(1000);}
-								catch(InterruptedException e){
-
-								}
+							// create datasets
+							dataset = new XYSeriesCollection();	
+							
+							dataset2 = new XYSeriesCollection();
+							
+							data4 = new TaskSeriesCollection();
+							dataset4 = new XYTaskDataset(data4);
+							
+							
+							// create charts
+							chart = ChartFactory.createXYLineChart("CPU Usage", "Time", "Percentage", dataset, PlotOrientation.VERTICAL, true, true, false);
+							currChart = chart;
+							
+							plot1 = (XYPlot) chart.getPlot();
+							domain1 = (NumberAxis) plot1.getDomainAxis();
+							domain1.setRange(0, 10);
+							
+							chart2 = ChartFactory.createXYLineChart("Memory Usage", "Time", "Percentage", dataset, PlotOrientation.VERTICAL, true, true, false);
+							
+							chart4 = ChartFactory.createXYBarChart("Critical Sections", "Thread", false, "Time", dataset4, PlotOrientation.HORIZONTAL, true, true, false);
+							
+							
+							//create labels for the critical sections chart y-axis
+							String[] chart4AxisLabels = new String[(int)getNumberOfThreads()];
+							for(int j=0; j<chart4AxisLabels.length; j++){
+								chart4AxisLabels[j] = "Thread " + (j+1);
 							}
-						}
-					});
-					thread.run();
-					
+							SymbolAxis symbolaxis = new SymbolAxis("Series", chart4AxisLabels);
+							
+							//set the domain and range of the critical sections axes
+							plot4 = chart4.getXYPlot();
+							plot4.setDomainAxis(symbolaxis);
+							plot4.setRangeAxis(new DateAxis("Time"));
+							
+							//not entirely sure why we need these three lines but its started working when I put them in
+							renderer4 = (XYBarRenderer) plot4.getRenderer();
+							renderer4.setUseYInterval(true);
+							ChartUtilities.applyCurrentTheme(chart4);
+							
+							
+							//this is for chart4 *****************************************************************************
+							taskSeriesArray = new TaskSeries[(int)getNumberOfThreads()];										//****
+							for(int i=0; i<taskSeriesArray.length; i++){												//****
+								taskSeriesArray[i] = new TaskSeries("Thread" + Integer.toString(i+1));					//****
+																														//****
+								//The Task object has the parameters Task(String, Date start, Date end)					//****
+								//The class Date represents a specific instant in time, with millisecond precision.		//****
+								//Make sure to cast the Date parameter to a long 										//****
+								taskSeriesArray[i].add(new Task("herro", new Date(2*(i+1)), new Date(3*(i+1))));				//****
+								data4.add(taskSeriesArray[i]);															//****
+							}																							//****
+							//************************************************************************************************
+							
+							// create a panel to put the chart in
+							chartPanel = new ChartPanel(chart);
+							
+							//This is for chart1 *****************************************************
+							seriesArray = new XYSeries[(int)getNumberOfThreads()];                     //**
+							for(int i = 0; i<seriesArray.length; i++){                            //**
+								seriesArray[i] = new XYSeries("Thread" + Integer.toString(i+1));  //**
+								dataset.addSeries(seriesArray[i]);								  //**
+							}																	  //**
+							//************************************************************************
+							
+							chartPanel.setVisible(true);
 								
+							
+							//creating JButtons for JPanel 1
+							JButton button1 = new JButton("CPU Visualizer");
+							JButton button2 = new JButton("Memory Visualizer");
+							JButton button3 = new JButton("Thread Sleep Time");
+							JButton button4 = new JButton("Thread Critical Sections");
+							
+										
+							button1.setMaximumSize(new Dimension(200, 200));
+							button2.setMaximumSize(new Dimension(200, 200));
+							button3.setMaximumSize(new Dimension(200, 200));
+							button4.setMaximumSize(new Dimension(200, 200));
+							
+							//add action listeners to each button
+							button1.addActionListener(new ActionListener(){
+								public void actionPerformed(ActionEvent e){
+									chartPanel = new ChartPanel(chart);
+									jpanel2.removeAll();
+									jpanel2.add(chartPanel);
+									jpanel2.revalidate();
+									jpanel2.repaint();
+									currChart = chart;
+								}
+							});
+							
+							
+							
+							
+							button2.addActionListener(new ActionListener(){
+								public void actionPerformed(ActionEvent e){
+									chartPanel2 = new ChartPanel(chart2);
+									jpanel2.removeAll();
+									jpanel2.add(chartPanel2);
+									jpanel2.revalidate();
+									jpanel2.repaint();
+									currChart = chart2;
+								}
+							});
+							
+							
+							button3.addActionListener(new ActionListener(){
+								public void actionPerformed(ActionEvent e){
+									//add code here
+								}
+							});
+							
+							
+							button4.addActionListener(new ActionListener(){
+								public void actionPerformed(ActionEvent e){
+									chartPanel4 = new ChartPanel(chart4);
+									jpanel2.removeAll();
+									jpanel2.add(chartPanel4);
+									jpanel2.revalidate();
+									jpanel2.repaint();
+									currChart = chart4;
+								}
+							});
+							
+							
+							
+							
+							
+							//creating JPanels
+							jpanel1 = new JPanel();
+							jpanel1.setLayout(new BoxLayout(jpanel1, BoxLayout.Y_AXIS));
+							
+							jpanel2 = new JPanel();
+							jpanel3 = new JPanel();			
+							
+							//adding buttons to JPanel1
+							jpanel1.add(button1);
+							jpanel1.add(button2);
+							jpanel1.add(button3);
+							jpanel1.add(button4);
+							
+							jpanel1.setVisible(true);
+							
+							
+							//adding the chartPanel with the JFreeChart inside to JPanel2
+							jpanel2.add(chartPanel);
+							jpanel2.setVisible(true);
+							
+							//adding checkboxes to jpanel3
+							int numThreads = (int) getNumberOfThreads();
+							checkboxes = new JCheckBox[numThreads];
+							
+							int i;
+							for(i=0; i<numThreads; i++){
+								checkboxes[i] = new JCheckBox("Thread " + Integer.toString(i+1), true);
+								checkboxes[i].addActionListener(new ActionListener(){
+									public void actionPerformed(ActionEvent e){
+										//remove data series here
+										JCheckBox thisBox = (JCheckBox) e.getSource();
+										if(thisBox.isSelected() == false){
+											removeSeries(thisBox.getText());
+										}
+										else{
+											addSeries(thisBox.getText());
+										}
+									}
+								});
+								jpanel3.add(checkboxes[i]);
+							}
+							jpanel3.setVisible(true);
+							
+							
+							
+							//Adding components to JFrame using GridBagLayout manager
+							jframe = new JFrame("Concurrent Visualizer");
+							jframe.setLayout(new GridBagLayout());
+							jframe.setResizable(false);
+							jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+							
+							gc = new GridBagConstraints();
+							gc.gridx = 0;
+							gc.gridy = 0;
+							gc.gridwidth = 1;
+							gc.gridheight = 1;
+							gc.weightx = 10;
+							gc.weighty = 10;
+							gc.insets = new Insets(0, 0, 0, 0);
+							gc.anchor = GridBagConstraints.FIRST_LINE_START;
+							gc.fill = GridBagConstraints.BOTH;
+							
+							
+							gc.gridheight = 3;
+							gc.gridwidth = 1;
+							jframe.add(jpanel1, gc);
+							
+							
+							gc.gridx = 1;
+							gc.gridy = 0;
+							gc.gridheight = 2;
+							gc.gridwidth = 2;
+							jframe.add(jpanel2, gc);
+							
+							gc.gridx = 1;
+							gc.gridy = 2;
+							gc.gridheight = 1;
+							gc.gridwidth = 2;
+							jframe.add(jpanel3, gc);
+							
+							
+							jframe.setSize(900, 600);
+							jframe.setVisible(true);
+							
+							
+							Thread thread = new Thread(new Runnable(){
+								public void run(){
+									int counter = 0;
+									int i;
+									while(counter < 100){
+										for(i=0; i<seriesArray.length; i++){
+											seriesArray[i].add(counter, returnRandom());
+											if(counter > 10)
+												domain1.setRange(counter-10, counter);
+										}
+										counter++;
+										try{Thread.sleep(1000);}
+										catch(InterruptedException e){
+
+										}
+									}
+								}
+							});
+							thread.run();
+							
+				
+						}
+					});		
+					
+					guiThread.start();
 	}
 	
 	
@@ -314,9 +320,9 @@ public class VisualGUI {
 	
 	
 	//This method gets the number of threads from the library and returns it
-	public static int getNumberOfThreads(){
-		//return Visualizer.getTotalThreads();
-		return 7;
+	public static long getNumberOfThreads(){
+		return Visualizer.threadCount();
+		//return 7;
 	}
 	
 	
